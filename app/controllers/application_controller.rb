bb31139689c55a2e7fcea6e83c3c10f35e4fef2b
Ethan <ethan_gustafson@outlook.com>
2020-01-12 22:40:13 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,41 +8,57 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
-  get "/" do
+  get "/" do # get "/" do renders an index.erb file with links to signup or login.
     erb :index
   end
 
-  get "/signup" do
-    erb :signup
+  get "/signup" do # get '/signup' renders a form to create a new user.
+    erb :signup # The form includes fields for username and password.
   end
 
-  post "/signup" do
-    #your code here
-
+  post "/signup" do # creates a new user object with a username and a password
+     @user = User.new(username: params[:username], password: params[:password])
+     
+    if @user.save
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
   end
 
-  get '/account' do
-    @user = User.find(session[:user_id])
+  get '/account' do # get '/account' renders an account.erb page,
+    @user = User.find(session[:user_id]) # which should be displayed once a user successfully logs in.
     erb :account
   end
 
 
-  get "/login" do
+  get "/login" do # get '/login' renders a form for logging in.
     erb :login
   end
 
-  post "/login" do
-    ##your code here
+  post "/login" do ##your code here
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password]) # if user is true and user.authenticates the password
+      session[:user_id] = user.id # We set the session hash user_id = to the user.id
+      redirect '/account'
+    else
+      redirect '/failure'
+    end
   end
 
-  get "/failure" do
-    erb :failure
+  get "/failure" do # get '/failure' renders a failure.erb page. 
+    erb :failure # This will be accessed if there is an error logging in or signing up.
   end
 
-  get "/logout" do
+  get "/logout" do # get '/logout' clears the session data and redirects to the home page.
     session.clear
     redirect "/"
   end
+
+  # The Helper Methods at the bottom of the controller are part of Sinatra's configurations for helper methods. 
+  # These are methods that allow us to add logic to our views. 
+  # Views automatically have access to all helper methods thanks to Sinatra.
 
   helpers do
     def logged_in?
--- db/migrate/20200113033443_createusers.rb
+++ db/migrate/20200113033443_createusers.rb
@@ -1,7 +1,11 @@
-class User < ActiveRecord::Base
-has_secure_password
+class Createusers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |c|
+      c.string :username
+      c.string :password_digest
+    end
+  end
 end
-
 # Note that even though our database has a column called password_digest, 
 # we still access the attribute of password. 
 # This is given to us by has_secure_password
